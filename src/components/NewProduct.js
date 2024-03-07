04aee5918173a29adf5283a32e7e636965d89eb4
FouadALLAOUI <allaoui70fouad71@gmail.com>
2024-03-07 12:47:00 +0100

    Data Bindding btw values and inputs

--- src/components/NewProduct.js
+++ src/components/NewProduct.js
@@ -1,6 +1,11 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+//Pay Attention : JSX != HTML+CSS
 function NewProduct() {
+  
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState(0);
+  const [checked, setChecked] = useState(false);
+  
   return (
     <div className='row p-1'>
       <div className='col-md-6'>
@@ -9,14 +14,21 @@ function NewProduct() {
             <form >
               <div className='mb-3'>
                 <label className='form-lable'>Name : </label>
-                <input className='form-controle'></input>
+                <input onChange={(e)=>setName(e.target.value)} 
+                 value={name}//Tow way Binding
+                className='form-controle'/>
               </div>
               <div className='mb-3'>
                 <label className='form-lable'>Price : </label>
-                <input className='form-controle'></input>
+                <input onChange={(e)=>setPrice(e.target.value)} 
+                 value={price}//Tow way Binding 
+                 className='form-controle'></input>
               </div>
               <div className="form-check">
-                <input className="form-check-input" type="checkbox"/>
+                <input className="form-check-input" type="checkbox" 
+                onChange={(e)=>setChecked(e.target.value)} 
+                 checked={checked}//Tow way Binding
+                 />
                   <label className="form-check-label" for="flexCheckChecked">
                     Checked
                   </label>
