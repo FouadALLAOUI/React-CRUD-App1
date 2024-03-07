67e302f368d5676afcff06d76d8decabbddebba8
FouadALLAOUI <allaoui70fouad71@gmail.com>
2024-03-07 11:42:13 +0100

    Add all crud api function (best practices)

--- src/app/app.js
+++ src/app/app.js
@@ -7,3 +7,23 @@ export const productsApi = axios.create({
 const getProducts=()=>{
     return productsApi.get("/products");
 }
+
+const deleteProducts=(product)=>{
+    return productsApi.delete(`/products/${product.id}`);
+}
+
+const getProduct=(id)=>{
+    return productsApi.get(`/products/${id}`);
+}
+
+const saveProduct=(product)=>{
+    return productsApi.post(`/products`, product);
+}
+
+const checkProduct=(product)=>{
+    return productsApi.patch(`/products`,{checked: !product.checked});
+}
+
+const updateProduct=(product)=>{
+    return productsApi.put(`/products`,product);
+}
